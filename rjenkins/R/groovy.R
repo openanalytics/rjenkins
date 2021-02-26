2096023c6314a9750d387669c380b4ec027b4fc8
Daan Seynaeve <daan.seynaeve@openanalytics.eu>
2021-02-26 17:17:50 +0100

    fix: nested groovy calls

--- rjenkins/R/groovy.R
+++ rjenkins/R/groovy.R
@@ -11,7 +11,7 @@ GroovyCall <- function(.name, ...) {
           name = .name,
           arguments = list(...)
       ),
-      class = c("GrooyCall", "list")
+      class = c("GroovyCall", "list")
   )
   
 }
@@ -136,6 +136,8 @@ formatArgument <- function(value) {
     formatGString(value)
   } else if (is(value, "GroovyClosure")) {
     formatGroovyClosure(value)
+  } else if (is(value, "GroovyCall")) {
+    formatGroovyCall(value)
   } else {
     formatGString(GString(value))
   }
